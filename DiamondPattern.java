f1d5664263e98f9e3f04085552bcdc92d6d3f91c
tavi7842 <117448678+tavi7842@users.noreply.github.com>
2023-02-08 19:34:41 +0530

    Control_Statements

---
+++ DiamondPattern.java
@@ -0,0 +1,54 @@
+/* JFM1T4_Assignment5:
+   Write a program to print the following pattern:
+       a
+     a b a
+   a b c b a
+     a b a
+       a
+*/
+//Add main method here
+//Declare variables i,j,k,m and initialize ch=97(because we are printing alphabet a)
+//use for loop and if condition to print a,b,c 
+public class DiamondPattern {
+        public static void main(String args[]){
+              int i,j,k,m,ch=97;
+        //using for loop
+              for(i=1;i<=3;i++)
+              {
+                 for(j=1;j<=3-i;j++)
+                 {
+                    System.out.print("  ");
+                 }
+
+        //using for loop and if condition
+                 for(k=1;k<=(2*i)-1;k++)
+                 {
+                    if(k<i)
+                       System.out.print((char)ch+++" ");
+                    else if(k==i)
+                       System.out.print((char)ch+" ");
+                    else
+                       System.out.print((char)--ch+" ");
+                 }
+                  System.out.println();
+             }
+
+             for(i=2;i>=1;i--)
+             {
+                for(j=1;j<=3-i;j++)
+                {
+                 System.out.print("  ");
+                 }
+                 for(m=1;m<=(2*i)-1;m++)
+                 {
+                    if(m<i)
+                       System.out.print((char)ch+++" ");
+                    else if(m==i)
+                       System.out.print((char)ch+" ");
+                    else
+                       System.out.print((char)--ch+" ");
+                 }
+                    System.out.println();
+              }
+           }
+        }
---
+++ HighestNumber.java
@@ -0,0 +1,48 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class HighestNumber {
+     
+//Add main method
+public static void main(String args[]){
+  
+
+//Declare the three variables
+int a=0,b=0,c=0;
+//Use the scanner class to provide input at execution time
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ a =s.nextInt();
+ System.out.println("Enter the Second number");
+ b =s.nextInt();
+ System.out.println("Enter the Third number");
+ c =s.nextInt();
+ 
+
+
+//check which number is highest using if else condition
+//Print the highest of three numbers
+  if (a > b && a > c){
+    System.out.println(a+" "+"is highest number");
+  } else if(b>c){
+    System.out.println(b+" "+"is highest number");
+  } else {
+     System.out.println(c+" "+"is highest number");
+  }
+
+ }
+}
